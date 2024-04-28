5059dd8ff44424e715691f9b06368552034dbe35
Yamen Malik <60931606+Yamen-Malik@users.noreply.github.com>
2024-04-28 09:57:13 +0300

    Completed pipeline code
    Finished integrating the desertification_analyzer with the qaoa_jobscheduler

--- app/desertification_analyzer.py
+++ app/desertification_analyzer.py
@@ -1,77 +1,110 @@
 import pandas as pd
 import numpy as np
 
+
 class DesertificationAnalyzer:
-    def __init__(self, nvdi_data, lst_data, models):
-        self.nvdi_data = nvdi_data
-        self.lst_data = lst_data
-        self.models = models
+    def __init__(self, data):
+        self.nvdi_data = data["nvdi_data"]
+        self.lst_data = data["lst_data"]
+        self.models = data["models"]
         self.features = None
         self.samples = None
 
-    def _get_features_sample_size(self,data_choices):
+    def _init_features_sample_size(self, data_choices: list[str]):
+        """Initialize the values for the model features and samples"""
         for data_choice in data_choices:
-            if data_choice == 'nvdi':
+            if data_choice == "nvdi":
                 nvdi_data = pd.read_csv(self.nvdi_data)
-            elif data_choice == 'lst':
+            elif data_choice == "lst":
                 lst_data = pd.read_csv(self.lst_data)
+
         # join the data
         data = pd.concat([nvdi_data, lst_data], axis=1)
-        samples, features  =  data.shape
+        samples, features = data.shape
+
         self.samples = samples
         self.features = features
-        return samples, features
 
-    def random_forest_resource_estimation(self, data_choices, num_estimators = 100):
+    def random_forest_resource_estimation(self, num_estimators: int = 100):
+        """Determine the run time via complexity of the random forest model
+
+        Returns:
+            int: the estimated computational cost
+        """
         print("Running Random Forest")
-        # we determine the run time via complexity of the model
-        computational_cost = num_estimators *self.samples * self.features * np.log(samples)
+        computational_cost = (
+            num_estimators * self.samples * self.features * np.log(self.samples)
+        )
         return computational_cost
 
-    def gradient_boosted_trees_resource_estimation(self,data_choices,tree_num=100, depth=100):
-        print("Running Gradient Boosted Trees")
+    def gradient_boosted_trees_resource_estimation(
+        self, tree_num: int = 100, depth: int = 100
+    ):
         """
-        The Big O complexity of training gradient boosted trees generally depends on several factors including the number of trees in the ensemble, the depth of each tree, and the number of data points. Here's a breakdown:
+        The complexity of training gradient boosted trees depends on several factors including:
 
         Number of Trees (M): If you have more trees, it takes more time to train each additional tree, adding linearly to the complexity.
         Depth of Trees (D): Trees that are deeper have more nodes and take longer to compute. The depth typically depends on the number of features and the specific configuration of the model. The time to train a tree grows exponentially with depth in the worst case, as deeper trees can potentially split into more branches.
         Number of Data Points (N): More data points mean more computations for splitting nodes at each level of each tree.
-        Given these parameters, the Big O complexity of training gradient boosted trees is generally O(M * N * 2^D). This assumes that each tree is built sequentially, and the depth of the trees can lead to exponential growth in complexity dependi
-        """ 
-        samples, _features = self._get_features_sample_size(data_choices)
-        # we determine the run time via complexity of the model
-        computational_cost = tree_num * samples * 2**depth
-        return computational_cost
-    
-    def support_vector_machines_resource_estimation(self):
-        print("Running Support Vector Machines")
-        # we determine the run time via complexity of the model
-    
-    def cnn_resource_estimation(self):
-        print("Running CNN")
 
-    
+        Returns:
+            int: the estimated computational cost
+        """
+        print("Running Gradient Boosted Trees")
+        computational_cost = tree_num * self.samples * 2**depth
+        return computational_cost
+
+    def support_vector_machines_resource_estimation(self):
+        """
+        The Big O complexity of training support vector machines generally depends on several factors including the number of trees in the ensemble, the depth of each tree, and the number of data points.
+
+        commonly SVM algorithms has a time complexity of O(n^2)
+
+        Returns:
+            int: the estimated computational cost
+        """
+        print("Running Support Vector Machines")
+        computational_cost = self.samples**2
+        return computational_cost
+
+    def cnn_resource_estimation(self, k: int = 10, m: int = 23):
+        """
+        For a CNN layer with a single convolution operation, the time complexity per layer can be expressed as O(n^2 * k^2 * m), where:
+
+        n is the size of the input feature map (width * height),
+        k is the size of the convolutional kernel (width * height),
+        m is the number of output channels.
+
+        Returns:
+            int: the estimated computational cost
+        """
+        print("Running CNN")
+        computational_cost = self.features**2 * k**2 * m
+        return computational_cost
+
     def estimate_resources(self):
+        """Returns an estimate of computational cost for all models as a dictionary.
+
+        Returns:
+            dict: the models and their normalized estimated computational cost as a key-value pairs
+        """
+
         results = {}
+        data_choices = ["nvdi", "lst"]
+        self._init_features_sample_size(data_choices)
+
         for model in self.models:
             if model == "Random Forest":
-                results[model] = self.random_forest_resource_estimation(['nvdi', 'lst'])
+                results[model] = self.random_forest_resource_estimation()
             elif model == "Gradient Boosted Trees":
-                results[model] = self.gradient_boosted_trees_resource_estimation(['nvdi', 'lst'])
+                results[model] = self.gradient_boosted_trees_resource_estimation()
             elif model == "Support Vector Machines":
                 results[model] = self.support_vector_machines_resource_estimation()
             elif model == "CNN":
                 results[model] = self.cnn_resource_estimation()
+
         # normalize the results
         max_value = max(results.values())
-        results = {key: value/max_value for key, value in results.items()}
-        {
-            'Random Forest': 0.5,
-            'Gradient Boosted Trees': 0.8,
-            'Support Vector Machines': 0.2,
-            'CNN': 1.0
-        }
+        results = {key: value / max_value for key, value in results.items()}
 
-        
         return results
-
--- app/main.py
+++ app/main.py
@@ -1,37 +1,56 @@
-from desertification_analyzer  import DesertificationAnalyzer
+from desertification_analyzer import DesertificationAnalyzer
 from qaoa_jobscheduler import QAOAJobScheduler
 
+
 def get_user_input():
+    """Prompts the user to enter values for the following:
+    country, nvdi_data, lst_data, model
+
+    Returns:
+        tuple: the user-set values (country, nvdi_data, lst_data, model_selections)
+    """
     country = input("Please provide the country for desertification analysis: ")
     nvdi_data = input("Please provide the NDVI data for desertification analysis: ")
     lst_data = input("Please provide the LST data for desertification analysis: ")
-    models_available = ["Random Forest", "Gradient Boosted Trees", "Support Vector Machines", "CNN"]
+    models_available = [
+        "Random Forest",
+        "Gradient Boosted Trees",
+        "Support Vector Machines",
+        "CNN",
+    ]
+
     print("Choose models (separated by commas):")
     for index, model in enumerate(models_available, 1):
         print(f"{index}. {model}")
-    
+
     models_indices = input("Enter the numbers of the models you choose: ")
+
     # Convert indices to int and subtract 1 to match list indices
-    model_selections = [models_available[int(index.strip()) - 1] for index in models_indices.split(',')]
+    model_selections = [
+        models_available[int(index.strip()) - 1] for index in models_indices.split(",")
+    ]
     return country, nvdi_data, lst_data, model_selections
 
+
 def main():
+    """Calls the analyzer model to get estimated resources, then passes it to the scheduler to get the execution schedule over multiple machines"""
     country, nvdi_data, lst_data, model_selections = get_user_input()
     data = {
-        'country': country,
-        'nvdi_data': nvdi_data,
-        'lst_data': lst_data,
-        'models': model_selections
+        "country": country,
+        "nvdi_data": nvdi_data,
+        "lst_data": lst_data,
+        "models": model_selections,
     }
     analyzer = DesertificationAnalyzer(data)
-    analyzer.estimate_resources()
-    # Pass the analyzer object to the QAOAJobScheduler
-    scheduler = QAOAJobScheduler(analyzer)
+    resources = analyzer.estimate_resources()
+
+    # Pass the estimated resources to the QAOAJobScheduler
+    scheduler = QAOAJobScheduler(resources)
     result = scheduler.run()
+
     # depending on the result run on the available GPU
     # or run on the available CPU
     print(result)
-
 
 
 if __name__ == "__main__":
